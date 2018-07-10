460e93d21de3fba51c7ea190acd5c64388b7f869
Sotatek-DucPham <duc.pham@sotatek.com>
2018-07-10 11:50:31 +0700

    path require

--- backend/app/controllers/AppController.js
+++ backend/app/controllers/AppController.js
@@ -1,6 +1,7 @@
 const _                       = require('lodash');
 const ExternalServiceAdapter  = require('sota-core').load('external_service/foundation/ExternalServiceAdapter');
 const BaseController          = require('sota-core').load('controller/BaseController');
+const path                    = require('path');
 
 module.exports = BaseController.extends({
   classname: 'AppController',
